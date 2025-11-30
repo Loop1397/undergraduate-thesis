668df0964257221a55be9b5cdbe09a4d53d33527
Loop1397 <shwlsdn0906@gmail.com>
2025-12-01 06:07:46 +0900

    refactor: component를 별도의 파일로 이동

--- src/master-tree/master-tree.page.tsx
+++ src/master-tree/master-tree.page.tsx
@@ -1,43 +1,9 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import { printJsonData } from "./master-tree.util";
+import { TreeWrapper, AdvisorTree, TreeRow, TreeNode } from "./master-tree.component"
 
-const TreeWrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #cf6a87;
-`;
-
-// grid=template-columnsにより各rowを何個で分けるかを決める
-// --colsは下で計算する
-const AdvisorTree = styled.div`
-  display: grid;
-  grid-template-columns: repeat(var(--cols, 1), minmax(0, 1fr));
-  gap: 16px;
-`;
-
-// grid-template-columns: subgrid;でAdvisorTree, AdviseeTreeのgrid-template-columnsの値を取ってくる
-const TreeRow = styled.div`
-  background-color: #574b90;
-  display: grid;
-  grid-template-columns: subgrid;
-  grid-column: 1 / -1;
-  min-height: 50px;
-`;
-
-const TreeNode = styled.div<{
-  $start?: number;
-  $end?: number;
-  $span?: number;
-}>`
-  background-color: rgb(171, 128, 211);
-  min-width: 50px;
-  grid-column: ${({ $start, $end, $span }) => {
-    if ($start != null && $end != null) return `${$start} / ${$end}`;
-    if ($start != null && $span != null) return `${$start} / span ${$span}`;
-    return "auto";
-  }};
-`;
+// import JsonData 
+import researcherData from '../../data.json';
 
 function MasterTree() {
   type Researcher = {
