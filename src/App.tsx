3658fed324a4eff192efd5a0ca0ec7b96cbe4996
Loop <shwlsdn0906@gmail.com>
2025-12-30 18:15:53 +0900

    style: header 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import MasterTree from "./master-tree/master-tree.page";
+import Header from "./header/header.page";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div className="container">
-      <MasterTree />
-    </div>
+    <>
+      <Header></Header>
+      <div style={{ width: "100%", height: "1px", backgroundColor: "#eeeeee" }}></div>
+      <div className="container" style={{ backgroundColor: "#F3F7F9" }}>
+        <MasterTree />
+      </div>
+    </>
   );
 }
 
